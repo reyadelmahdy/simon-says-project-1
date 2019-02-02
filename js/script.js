ad3c90d0d6d56ecf2270eb03e1ad76b7b78f140a
Alex <alexthirlwell123@gmail.com>
2019-02-02 10:30:58 +0000

    Added functions to allow coloured lights to flash

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@ let on = false;
 let win;
 let order = [];
 let level;
-
+let light;
+let intervalId;
+let compTurn;
 
 const onButton = document.querySelector("#on");
 const levelCounter = document.querySelector("#level");
 const gameStart = document.querySelector('.start');
-
+const greenSection = document.querySelector("#greensection");
+const redSection = document.querySelector("#redsection");
+const yellowSection = document.querySelector("#yellowsection");
+const blueSection = document.querySelector("#bluesection");
 
 onButton.addEventListener('click', (event) => {
   if (onButton.checked == true) {
@@ -24,13 +29,71 @@ gameStart.addEventListener('click', (event) => {
   }
 });
 
-function play(){
-    win = false;
-    order = [];
-    level = 1;
-    levelCounter.innerHTML = 1;
-    for (var i = 0; i < 20; i++) { 
-    order.push(Math.floor(Math.random() * 4) + 1);
+function play() {
+  win = false;
+  order = [];
+  light = 0;
+  intervalId = 0;
+  level = 1;
+  levelCounter.innerHTML = 1;
+  for (var i = 0; i < 20; i++) {
+    order.push(Math.floor(Math.random() * 4) + 1); 
   }
+  
   console.log(order);
+  compTurn = true;
+
+  intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+  on = false;
+
+  if (light == level) {
+    clearInterval(intervalId);
+    compTurn = false;
+    darkColor();
+    on = true;
+  }
+//aligns the numbers 1-4 with the different segments and calls the function to light the correct sequence of colours
+  if (compTurn) {
+    darkColor();
+    setTimeout(() => {
+      if (order[light] == 1) green();
+      if (order[light] == 2) red();
+      if (order[light] == 3) yellow();
+      if (order[light] == 4) blue();
+      light++;
+    }, 200);
+  }
+}
+
+function green() {
+  greenSection.style.backgroundColor = "lightgreen";
+}
+
+function red() {
+  redSection.style.backgroundColor = "tomato";
+}
+
+function yellow() {
+  yellowSection.style.backgroundColor = "yellow";
+}
+
+function blue() {
+  blueSection.style.backgroundColor = "lightskyblue";
+}
+
+function darkColor() {
+  greenSection.style.backgroundColor = "darkgreen";
+  redSection.style.backgroundColor = "darkred";
+  yellowSection.style.backgroundColor = "goldenrod";
+  blueSection.style.backgroundColor = "darkblue";
+}
+
+function lightColor() {
+  greenSection.style.backgroundColor = "lightgreen";
+  redSection.style.backgroundColor = "tomato";
+  yellowSection.style.backgroundColor = "lightyellow";
+  blueSection.style.backgroundColor = "lightskyblue";
 }
