195337a555bd2cf95a4e80156d3b612cfac222f9
Alex <alexthirlwell123@gmail.com>
2019-02-02 09:32:14 +0000

    Added 'play' function, tested with 'console.log(order)'

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 let on = false;
+let win;
+let order = [];
+let level;
 
-const levelCounter = document.querySelector("#level");
+
 const onButton = document.querySelector("#on");
-
+const levelCounter = document.querySelector("#level");
+const gameStart = document.querySelector('.start');
 
 
 onButton.addEventListener('click', (event) => {
   if (onButton.checked == true) {
     on = true;
-    levelCounter.innerHTML = "-";
   } else {
     on = false;
     levelCounter.innerHTML = "";
   }
 });
+
+gameStart.addEventListener('click', (event) => {
+  if (on || win) {
+    play();
+  }
+});
+
+function play(){
+    win = false;
+    order = [];
+    level = 1;
+    levelCounter.innerHTML = 1;
+    for (var i = 0; i < 20; i++) { 
+    order.push(Math.floor(Math.random() * 4) + 1);
+  }
+  console.log(order);
+}
